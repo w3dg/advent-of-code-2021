bb9ec26c55e7ffff2cceb83e4fb10850a6392335
DG <debopamgupta3000@gmail.com>
2021-12-08 10:11:25 +0530

    Turn for loop to formula save computations

--- day7/utils.js
+++ day7/utils.js
@@ -2,10 +2,5 @@
 
 // NOTE: recursive version took quite long, never the less for loop also takes too long. idk.
 export function calcFuelWithIncrease(num) {
-  let sum = 0;
-  for (let i = 1; i <= num; i++) {
-    sum += i;
-  }
-
-  return sum;
+  return Math.floor((num * (num + 1)) / 2);
 }
