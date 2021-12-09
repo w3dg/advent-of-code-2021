bf0132248b797fe98297129f2ca0d1ae9ce5b61c
DG <51162936+debopamgupta@users.noreply.github.com>
2021-12-09 16:46:00 +0530

    :memo: Redo with OOP approach; setup for part 2

--- day9/day9.js
+++ day9/day9.js
@@ -1,39 +1,53 @@
 import { readFileSync } from "fs";
 
-import { utility } from "./utils";
+import { Point } from "./utils";
 
-const data = readFileSync(__dirname + "/sample.txt", "utf8").split("\r\n");
-// const data = readFileSync(__dirname + "/input.txt", "utf8").split("\r\n");
+// const data = readFileSync(__dirname + "/sample.txt", "utf8").split("\r\n");
+const data = readFileSync(__dirname + "/input.txt", "utf8").split("\r\n");
+const dataMatrix = data.map((row) => row.split("").map((x) => +x));
+let heightMapMatrix = [];
+
+for (let i = 0; i < dataMatrix.length; i++) {
+  let temp = [];
+  for (let j = 0; j < dataMatrix[i].length; j++) {
+    const point = new Point(i, j, dataMatrix[i][j]);
+    temp.push(point); // this makes them 0 based
+  }
+  heightMapMatrix.push(temp);
+}
 
 function puzzle1() {
-  const heightMapMatrix = data.map((row) => row.split("").map((x) => +x));
   const lowPoints = [];
-
   // if right, left, top bottom whatever available exists, then if its lowest than all of them, then its a low point
 
   for (let i = 0; i < heightMapMatrix.length; i++) {
     // rows
     for (let j = 0; j < heightMapMatrix[i].length; j++) {
       // cols
+      const currentPoint = heightMapMatrix[i][j];
       // grab whatever adjacent point is available
       let adjacents = [];
-      j - 1 >= 0 && adjacents.push(heightMapMatrix[i][j - 1]);
-      j + 1 < heightMapMatrix[i].length &&
-        adjacents.push(heightMapMatrix[i][j + 1]);
-      i - 1 >= 0 && adjacents.push(heightMapMatrix[i - 1][j]);
-      i + 1 < heightMapMatrix.length &&
-        adjacents.push(heightMapMatrix[i + 1][j]);
+      currentPoint.col - 1 >= 0 &&
+        adjacents.push(heightMapMatrix[i][currentPoint.col - 1]);
+      currentPoint.col + 1 < heightMapMatrix[i].length &&
+        adjacents.push(heightMapMatrix[i][currentPoint.col + 1]);
+      currentPoint.row - 1 >= 0 &&
+        adjacents.push(heightMapMatrix[currentPoint.row - 1][j]);
+      currentPoint.row + 1 < heightMapMatrix.length &&
+        adjacents.push(heightMapMatrix[currentPoint.row + 1][j]);
 
-      // check if the current i,j is lowest than the adjacent points
-      const currentPoint = heightMapMatrix[i][j];
-      const isHigher = (adjacent) => adjacent > currentPoint;
+      // check if the currentPoint is lowest than the adjacent points
+      const isHigher = (adjacent) => adjacent.height > currentPoint.height;
       if (adjacents.every(isHigher)) {
         lowPoints.push(heightMapMatrix[i][j]);
       }
     }
   }
 
-  const totalRiskLevel = lowPoints.reduce((acc, curr) => acc + curr + 1, 0);
+  const totalRiskLevel = lowPoints.reduce(
+    (acc, curr) => acc + curr.height + 1,
+    0
+  );
   return totalRiskLevel;
 }
 
--- day9/utils.js
+++ day9/utils.js
@@ -1,1 +1,12 @@
-export function utility() {}
+export class Point {
+  constructor(row, col, height) {
+    this.row = row;
+    this.col = col;
+    this.traversed = false;
+    this.height = height;
+  }
+
+  setTraversed() {
+    this.traversed = true;
+  }
+}
